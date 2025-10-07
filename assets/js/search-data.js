9999fc6c2c3ca19205db6b99186f8fca48b65f7c
DEVANSH-DVJ <devansh.dvj@gmail.com>
2025-10-07 05:07:35 +0000

    Deploying to gh-pages from @ act-compiler/act-compiler.github.io@64699ef7951277750780828bcf32f717eeb9481c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-publications",
+  },{id: "nav-tutorials",
+          title: "Tutorials",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/tutorials/";
+          },
+        },{id: "nav-publications",
           title: "Publications",
           description: "Publications in reverse chronological order.",
           section: "Navigation",
@@ -26,7 +33,12 @@ ninja.data = [{
         },{id: "news-our-paper-titled-taidl-tensor-accelerator-isa-definition-language-with-auto-generation-of-scalable-test-oracles-has-been-accepted-at-micro-2025-tada",
           title: 'Our paper titled TAIDL: Tensor Accelerator ISA Definition Language with Auto-generation of Scalable...',
           description: "",
-          section: "News",},{
+          section: "News",},{id: "tutorials-icfp-splash-2025",
+          title: 'ICFP/SPLASH 2025',
+          description: "In Singapore on Oct 12, 2025",
+          section: "Tutorials",handler: () => {
+              window.location.href = "/tutorials/splash2025/";
+            },},{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
