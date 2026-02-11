4aa1324f07513eeffec871c7f7fca3b01b8fd4e0
CharithYMendis <CharithYMendis@users.noreply.github.com>
2026-02-11 04:56:12 +0000

    Deploying to gh-pages from @ act-compiler/act-compiler.github.io@deff1129ae4f19b04fd9b4257354703e72fa4ad3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -36,13 +36,15 @@ ninja.data = [{
           section: "News",},{id: "news-we-organized-a-hands-on-tutorial-on-the-act-ecosystem-at-icfp-splash-2025-in-singapore",
           title: 'We organized a hands-on tutorial on the ACT Ecosystem at ICFP/SPLASH 2025 in...',
           description: "",
-          section: "News",},{id: "news-we-are-conducting-a-hands-on-tutorial-on-the-act-ecosystem-at-micro-2025-in-seoul-south-korea",
-          title: 'We are conducting a hands-on tutorial on the ACT Ecosystem at MICRO 2025...',
+          section: "News",},{id: "news-we-organized-a-hands-on-tutorial-on-the-act-ecosystem-at-micro-2025-in-seoul-south-korea",
+          title: 'We organized a hands-on tutorial on the ACT Ecosystem at MICRO 2025 in...',
           description: "",
-          section: "News",},{id: "news-we-are-conducting-a-hands-on-tutorial-on-the-act-ecosystem-at-asplos-2026-details-to-come-soon",
-          title: 'We are conducting a hands-on tutorial on the ACT Ecosystem at ASPLOS 2026....',
+          section: "News",},{id: "news-2026-02-10-asplos-tutorial",
+          title: '[2026 02 10] asplos Tutorial',
           description: "",
-          section: "News",},{id: "tutorials-asplos-2026",
+          section: "News",handler: () => {
+              window.location.href = "/news/%5B2026-02-10%5D%20asplos-tutorial.html";
+            },},{id: "tutorials-asplos-2026",
           title: 'ASPLOS 2026',
           description: "In Pittsburgh, USA on Mar 22, 2026",
           section: "Tutorials",handler: () => {
